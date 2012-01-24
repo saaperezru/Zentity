f58e4ca75bb6829c1e8aa69b7cea9fe618a1ffb9
saaperezru <saaperezru@unal.edu.co>
2012-01-24 20:33:04 +0000

    Base of Zentity interface for Modelado control is ready, extractors and latent topic scalar properties pending

--- python/Modelado_Control.py
+++ python/Modelado_Control.py
@@ -4,7 +4,9 @@ from scipy import mgrid
 from Modelo_Entidad import *
 import InterfazNMF
 import InterfazNMF_Control
-
+import InterfazZ_Entidad as ZEntidad
+import InterfazZ_Control as ZControl
+import Modelado_Entidad as Entidad
 class ControlMatrix
 
     @staticmethod
@@ -84,6 +86,73 @@ class ControlNMF:
         
 class ControlZentity:
     
-    def __init__(self):
-        pass
+    def __init__(self,DMMName,RTName,Control,topWords,LTNameTop,codeStoragePath,zxmlFilesPath):
+        """Builds an interface to communicate with InterfazZ to create a model with the detected LatentTopics
         
+        Attributes:
+        
+            - DMMName -- Name for the Data Model Module, it must be unique among the names of DMMs present in the Zentity instance
+            - RTName -- Name for the Resource Type that will shelter all the images in the collection. NOTE: It must be unque among all the Resource TYpes in all the Data Model Modules in the Zentity instance that will be used
+        """
+        self.codeStoragePath = codeStoragePath
+        self.zxmlDirectory = zxmlFilesPath
+        #Lets create the DMM
+        self.DMM = ZEntidad.DataModelModule(DMMName)
+        #And the only RT we will need
+        RT1 = ZEntidad.ResourceType(RTName)
+        #We must define an XMLStructure for the ZXML files
+        RT1.setXMLStructure(ZEntidad.XMLStructure("ParentImages","Image","Images"))
+        #As well as a VisualizationType
+        RT1.setVisualizationType(Entidad.VisualizationType(Entidad.VisualizationType.IMAGE,Control.collection.getImagesPath()))
+        #And all documents as instances of this DMM
+        for doc in Control.collection.getDocuments():
+            RT1.addInstance(doc.id)
+        #Finally we add this Resource Type to the DMM Model
+        DMM.addResourceType(RT1)
+        #Now add al  the Scalar Properties to this RT1
+        #Some basic SPs
+        titleExtractor = 
+        RT1.addProperty(ZEntidad.ScalarProperty("Title",ZEntidad.DataTYpes.STRING,titleExtractor,False,True)
+        descriptionExtractor =
+        RT1.addProperty(ZEntidad.ScalarProperty("Description",ZEntidad.DataTYpes.STRING,descriptionExtractor,False,True)
+        mainCategoryExtractor = 
+        RT1.addProperty(ZEntidad.ScalarProperty("Main_Category",ZEntidad.DataTYpes.STRING,mainCategoryExtractor)
+        #There is one SP for each Textual LatentTopic 
+        
+        #There is one SP for each Visual LatentTopic 
+        
+        #There is one SP for each ImportantTag
+        for tag in self.detectMostImportantTextualWords(topWords,LTNameTop):
+            #
+            tagExtractor = Entidad.TagExtractor(tag,)
+            RT1.addProperty(ZEntidad.ScalarProperty("Tag_"+tag,ZEntidad.DataTYpes.STRING,tagExtractor,False,True)
+        #Finally lets generate the Code Generator
+        self.Gen = Control.CodeGenerator(DMM,self.codeStoragePath)
+        
+
+    
+    def generateCode():
+        self.Gen.saveGenerationCode()
+        
+    def generateUploadingCode():
+        self.Gen.saveInsertionCode(self.zxmlDirectory)
+        
+    def generateZXMLFiles():
+        zxmlDirectory = "/home/tuareg/Documents/Work/Zentity/scripts/"
+        ZXML = Control.ZXMLGenerator(self.DMM,self.zxmlDirectory)
+        ZXML.save()
+        
+        
+    def detectMostImportantTextualWords(top,LTNamesTop):
+        """Detects the most important textual words according to this criterion:
+            * The <top> most used words are considered important
+            * The first <LTNamesTop> in the name of all the Latent Topics are important
+            
+            Attributes:
+                - top -- The number of words that will be considered important in a list of descendent images by 
+                - LTNamesTop -- The number of words that will be taken from the Latent TOpic names
+            Return:
+                A list of strings
+        """
+    
+        pass
