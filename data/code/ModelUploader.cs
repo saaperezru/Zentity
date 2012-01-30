4385d83855d7103c4768822f8cd7f489e7aafb50
Santiago Pérez <santiago1991@gmail.com>
2012-01-30 10:15:48 -0500

    Sligth correction to ModelUploader that must be included in the InterfazZ code

--- data/code/ModelUploader.cs
+++ data/code/ModelUploader.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Zentity.Core;
-using System.Xml.Serialization;using Zentity.DMMTest1;
+using System.Xml.Serialization;
+using Zentity.Modulo3;
+using System.Globalization;
+
 namespace DataInsert
 { 
  	 class DataInsert 
@@ -45,7 +48,7 @@ namespace DataInsert
  		 { 
  			dataParentImages= Deserialize<ParentImages>(xmlPath); 
  		 } 
- 		 public void insertData_RTTest1(ZentityContext zenContext, string xmlPath, string imgPath) 
+ 		 public void insertData_Entidad2(ZentityContext zenContext, string xmlPath, string imgPath) 
  		 { 
  			 loadParentImagesData(xmlPath); 
  			 using (ZentityContext context = zenContext) 
@@ -53,35 +56,32 @@ namespace DataInsert
  				 foreach (ParentImagesImage p in dataParentImages.Items) 
  				 { 
  					 //WARNING : Comparing attribute should be changed to a unique key 
- 					 context.MetadataWorkspace.LoadFromAssembly(System.Reflection.Assembly.Load("Zentity.DMMTest1")); 
- 					 var existingPres = (from pres in context.Resources 
- 						 where pres.ImageID.Equals(p.ImageID, StringComparison.OrdinalIgnoreCase) 
- 						 select pres).FirstOrDefault(); 
- 						 if (existingPres != null) 
- 						 { 
- 							 Console.WriteLine("[WARNING] Image {0} already exists in database", p.ImageID); 
- 							 continue; 
- 						 } 
+ 					 context.MetadataWorkspace.LoadFromAssembly(System.Reflection.Assembly.Load("Zentity.Modulo3")); 
  						 // Create resources. 
+                     NumberFormatInfo provider = new NumberFormatInfo();
+                     provider.NumberDecimalSeparator = ".";
  						 try 
  							 { 
- 							 RTTest1 img = new RTTest1 {
+ 							 Entidad3 img = new Entidad3 {
  								  Title = p.Title,
  								  Description = p.Description,
  								  Main_Textual_Category = p.Main_Textual_Category,
  								  Main_Visual_Category = p.Main_Visual_Category,
  								  ImageID = p.ImageID,
- 								  LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL = Convert.ToDouble(p.LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL),
- 								  LTT_TIERRA_AGUA_ANIMAL_USUARIO = Convert.ToDouble(p.LTT_TIERRA_AGUA_ANIMAL_USUARIO),
- 								  LTV_USUARIO_EJECUTIVO_PERSONA_TIERRA = Convert.ToDouble(p.LTV_USUARIO_EJECUTIVO_PERSONA_TIERRA),
- 								  LTV_TIERRA_AGUA_ANIMAL_PERSONA = Convert.ToDouble(p.LTV_TIERRA_AGUA_ANIMAL_PERSONA),
+ 								  LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL = Convert.ToDouble(p.LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL,provider),
+                                  LTT_TIERRA_AGUA_ANIMAL_USUARIO = Convert.ToDouble(p.LTT_TIERRA_AGUA_ANIMAL_USUARIO, provider),
+                                  LTV_USUARIO_EJECUTIVO_PERSONA_TIERRA = Convert.ToDouble(p.LTV_USUARIO_EJECUTIVO_PERSONA_TIERRA, provider),
+                                  LTV_TIERRA_AGUA_ANIMAL_PERSONA = Convert.ToDouble(p.LTV_TIERRA_AGUA_ANIMAL_PERSONA, provider),
  								  Tag_EJECUTIVO = (p.Tag_EJECUTIVO=="True"),
  								  Tag_PERSONA = (p.Tag_PERSONA=="True"),
  								  Tag_TIERRA = (p.Tag_TIERRA=="True"),
  								  Tag_AGUA = (p.Tag_AGUA=="True"),
  								  Tag_USUARIO = (p.Tag_USUARIO=="True"), 
- 							 }; 
- 							 context.AddToResources(img); 
+ 							 };
+                             Console.WriteLine("[INFO] Image " + img.ImageID + " - " + p.ImageID);
+                             Console.WriteLine("[INFO] LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL :  {0} - {1}", img.LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL,p.LTT_PERSONA_USUARIO_EJECUTIVO_ANIMAL);
+                             Console.Read();
+ 							 //context.AddToResources(img); 
  							 string[] imagesInFolder = Directory.GetFiles(imgPath, p.ImageID + ".*"); 
  							 // Create a Zentity file. 
  							 FileInfo ImageFile = new FileInfo(imagesInFolder[0]); 
@@ -93,19 +93,19 @@ namespace DataInsert
  							 fileResource.FileExtension = ImageFile.Extension; 
  							 fileResource.MimeType = "image/" + fileResource.FileExtension.Replace(".", string.Empty); 
  							 // Add the file to context.     
- 							 context.AddToResources(fileResource); 
+ 							 //context.AddToResources(fileResource); 
  							 Console.WriteLine("[INFO] Creating image {0}", p.ImageID); 
- 							 context.SaveChanges(); 
+ 							 //context.SaveChanges(); 
  							 // Now upload the actual binary content of the file.     
  							 FileStream fStream = new FileStream(ImageFile.FullName, FileMode.Open, FileAccess.Read); 
  							 Console.WriteLine("[INFO] Saving image {0} file", p.ImageID); 
- 							 context.UploadFileContent(fileResource, fStream); 
+ 							 //context.UploadFileContent(fileResource, fStream); 
  							 //Asociate file with Resource 
  							 img.Files.Add(fileResource); 
- 							 context.InsertResourceHasFile(fileResource.Id, img.Id); 
+ 							 //context.InsertResourceHasFile(fileResource.Id, img.Id); 
  							 //Save Changes in context 
  							 Console.WriteLine("[INFO] Associating image {0} and file", p.ImageID); 
- 							 context.SaveChanges(); 
+ 							 //context.SaveChanges(); 
  							 } 
  							 catch{ 
  								 Console.WriteLine("[ERROR] During image {0} creation", p.ImageID); 
@@ -117,18 +117,18 @@ namespace DataInsert
  		 static void Main(string[] args) 
  		 { 
  			 const string connectionString = @"provider=System.Data.SqlClient; 
- 				 metadata=../data/code/code/Zentity.DMMTest1.ExtendedCore.csdl|../data/code/code/Zentity.DMMTest1.csdl|../data/code/code/Zentity.DMMTest1.Consolidated.msl|../data/code/code/Zentity.DMMTest1.Consolidated.ssdl; 
+ 				 metadata=C:\\Users\\tuareg\\Desktop\\prueba4\\code\\Zentity.Modulo3.ExtendedCore.csdl|C:\\Users\\tuareg\\Desktop\\prueba4\\code\\Zentity.Modulo3.csdl|C:\\Users\\tuareg\\Desktop\\prueba4\\code\\Zentity.Modulo3.Consolidated.msl|C:\\Users\\tuareg\\Desktop\\prueba4\\code\\Zentity.Modulo3.Consolidated.ssdl; 
  				 provider connection string='Data Source=.; 
  				 Initial Catalog=Zentity;Integrated Security=True;MultipleActiveResultSets=True' 
  				 "; 
  			 DataInsert dataUploader = new DataInsert(); 
- 			 string imgFolderPath = "../data/images/"; 
- 			 string ZXMLPath = "../data/ZXML/"; 
+ 			 string imgFolderPath = "C:\\Users\\tuareg\\Desktop\\prueba4\\images\\"; 
+ 			 string ZXMLPath = "C:\\Users\\tuareg\\Desktop\\prueba4\\ZXML\\"; 
  			 string[] ZXMLFiles = Directory.GetFiles(ZXMLPath, "Images*.*"); 
  			 foreach (string a in ZXMLFiles) 
  			 { 
  				 ZentityContext zenContext = new ZentityContext(connectionString); 
- 				 dataUploader.insertData_RTTest1(zenContext, a, imgFolderPath); 
+ 				 dataUploader.insertData_Entidad2(zenContext, a, imgFolderPath); 
  			 }
 		 }
 	 }
